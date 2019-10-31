985a52cf88aa38956c9d86d63221e14ab6718d8e
aisair <ais11air@gmail.com>
2019-10-31 11:05:19 -0500

    Started Wheel of Fortune

---
+++ WoF.js
@@ -0,0 +1,22 @@
+let name1;
+let name2;
+var word;
+var board;
+var money = false;
+var myGuess;
+var choices = [];
+var rnum;
+var myMoney1 = 0;
+var myMoney2 = 0;
+var totguessed = 0;
+let pNum = 0;
+
+function makename(){ //starts game
+    name1 = document.getElementById("txtName1").value();
+    name2 = document.getElementById("txtName2").value();
+
+
+}
+function initword(){ //random word or phrase to guess
+
+}
